5d00b2c9857339b60986e1c3ed780a710057f50d
riley <riley.holmes@skayl.com>
2022-03-02 20:44:58 -0500

    Updates

---
+++ precache-manifest.87587f0123ad5df3cea280b31405ad05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4764d243ab180df51349b676c4596e56",
+    "url": "/hoge2-frontend/index.html"
+  },
+  {
+    "revision": "37ffd81c333901661629",
+    "url": "/hoge2-frontend/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "37ffd81c333901661629",
+    "url": "/hoge2-frontend/static/js/2.44b9efc4.chunk.js"
+  },
+  {
+    "revision": "d57dae176fb87a393a80e02d0e438720",
+    "url": "/hoge2-frontend/static/js/2.44b9efc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d642b5a2005c3d048e9b",
+    "url": "/hoge2-frontend/static/js/main.a8829dd4.chunk.js"
+  },
+  {
+    "revision": "aaa6a8d3c04e465f6c0c",
+    "url": "/hoge2-frontend/static/js/runtime-main.7021fecf.js"
+  }
+]);
